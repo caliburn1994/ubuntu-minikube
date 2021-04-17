c19ecc60880cebb67f368cf081ed2134edf7e389
Chen Xiaojie <caliburn1994@gmail.com>
2021-04-18 01:59:39 +0900

    SSH server

--- deploy/sub/ssh/ssh-server.sh
+++ deploy/sub/ssh/ssh-server.sh
@@ -14,7 +14,7 @@ if [ ! -f ~/.ssh/id_rsa ]; then
 fi
 
 if ! kubectl get service my-sshd &>/dev/null; then
-  echo "pubkey: "$(cat ~/.ssh/id_rsa.pub) | tee "${CURRENT_DIR}/values.yaml" >/dev/null
+  echo "pubkey: $(cat ~/.ssh/id_rsa.pub)" | tee "${CURRENT_DIR}/values.yaml" >/dev/null
   helm install my-sshd -f "${CURRENT_DIR}/values.yaml" "${CURRENT_DIR}/sshd"
 fi
 # kubectl port-forward --namespace default svc/my-sshd 2222:22 --address 0.0.0.0
