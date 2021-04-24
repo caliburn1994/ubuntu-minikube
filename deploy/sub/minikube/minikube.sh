4f186f9e2f221962a1138e26e71b17291d8f194d
xiaojie <caliburn1994@gmail.com>
2021-04-24 09:24:41 -0700

    fix issues

--- deploy/sub/minikube/minikube.sh
+++ deploy/sub/minikube/minikube.sh
@@ -11,9 +11,7 @@ echo_running
 if ! type -p kubectl &>/dev/null; then
   echo_debug "Installing kubectl..."
 
-  curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-  chmod +x ./kubectl
-  sudo mv ./kubectl /usr/local/bin/kubectl
+  curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" && chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl
   kubectl version --client
 
   # auto completion
@@ -44,36 +42,21 @@ if ! type -p minikube &>/dev/null; then
       wget https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz && sudo tar zxvf crictl-$VERSION-linux-amd64.tar.gz -C /usr/local/bin && rm -f crictl-$VERSION-linux-amd64.tar.gz
   fi
 
-  sudo systemctl enable kubelet.service
   swapoff -a # todo
 
   # auto completion
   # shellcheck disable=SC1090
   source <(minikube completion bash)
-  echo "source <(minikube completion bash)" >>~/.bashrc
+  echo "source <(minikube completion bash)" | tee -a ~/.bashrc
 
-  # sudo sysctl fs.protected_regular=0
-  sudo minikube start --vm-driver=none
-#  sudo mv /root/.kube /root/.minikube $HOME
-#  sudo chown -R $USER $HOME/.kube $HOME/.minikube
-#  sudo chgrp -R $USER $HOME/.minikube $HOME/.kube
-#  sudo chgrp -R $USER
-#  minikube delete
+  sudo sysctl fs.protected_regular=0
+  echo "export KUBECONFIG=/etc/kubernetes/kubelet.conf" | tee -a ~/.bashrc; export KUBECONFIG=/etc/kubernetes/kubelet.conf
+  sudo minikube start --vm-driver=none --addons=ingress
 
   # run minikube when boot up
-  # make_service "minikube.service" todo doesn't work
+  make_service "minikube.service"
 
   # run minikube dashboard when boot up
+  sudo minikube dashboard --url &
   make_service "minikube-dashboard.service"
-
-  # enable dashboard # todo need to test
-  minikube dashboard --url
 fi
-
-if ! minikube status >/dev/null 2>&1 ; then
-  export CHANGE_MINIKUBE_NONE_USER=true && minikube start --vm-driver=none
-fi
-
-make_service "minikube.service"
-
-echo_debug "If dashboard does not work, you should print this command: minikube dashboard"
