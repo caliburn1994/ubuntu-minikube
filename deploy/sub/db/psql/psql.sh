37d1d4ec370e32f9c72fa8a399236793d35ff870
xiaojie <caliburn1994@gmail.com>
2021-04-25 11:56:11 +0900

    fix issues

--- deploy/sub/db/psql/psql.sh
+++ deploy/sub/db/psql/psql.sh
@@ -9,35 +9,37 @@ POSTGRES_K8S_SERVICE="${POSTGRES_K8S_BASENAME}-postgresql" && export POSTGRES_K8
 
 echo_running
 # install
-if ! kubectl get service "${POSTGRES_K8S_SERVICE}" &>/dev/null; then
+if ! sudo kubectl get service "${POSTGRES_K8S_SERVICE}" &>/dev/null; then
   echo_debug "Installing PostgreSQL..."
   echo_debug "If helm version<3.1, use => helm install ${POSTGRES_K8S_BASENAME} bitnami/postgresql --version 10.2.2"
   echo_debug "If helm version>3.1, use => helm install ${POSTGRES_K8S_BASENAME} bitnami/postgresql"
-  helm repo add bitnami https://charts.bitnami.com/bitnami
-  helm install ${POSTGRES_K8S_BASENAME} bitnami/postgresql --version 10.2.2
+  sudo helm repo add bitnami https://charts.bitnami.com/bitnami
+  sudo helm install ${POSTGRES_K8S_BASENAME} bitnami/postgresql --version 10.2.2 -f "${CURRENT_DIR}/value.yaml"
 
   make_service "minikube-psql.service"
 fi
 
 # output result
-POSTGRES_PASSWORD="$(kubectl get secret --namespace default ${POSTGRES_K8S_SERVICE} -o jsonpath="{.data.postgresql-password}" | base64 --decode)"
+POSTGRES_PASSWORD="$(sudo kubectl get secret --namespace default ${POSTGRES_K8S_SERVICE} -o jsonpath="{.data.postgresql-password}" | base64 --decode)"
+# create database
+if ! type -p createdb &>/dev/null; then
+    # command tools
+  sudo apt-get install -y postgresql-client-common postgresql-client
 
-if ! kubectl get service "${POSTGRES_K8S_SERVICE}" &>/dev/null; then
-  # command tools
-  sudo apt-get install -y postgresql-client-common
-  sudo apt-get install -y postgresql-client
-
-  echo_info "For creating a new custom database,you should input this password: ${POSTGRES_PASSWORD}"
   # create new database
-  createdb -h localhost -p 5432 -U postgres testdb
+  export "PGPASSWORD=${POSTGRES_PASSWORD}" ; createdb -h localhost -p 5432 -U postgres testdb
 fi
 
 # config
-cat <<EOF >"${PROJECT_ROOT_PATH}/out/db/psql.properties"
-# custom
-url=jdbc:postgresql://localhost:5432/testdb
 # for gitlab
+cat <<EOF >"${PROJECT_ROOT_PATH}/out/db/psql.properties"
 url=jdbc:postgresql://localhost:5432/postgres
+username=postgres
+password=${POSTGRES_PASSWORD}
+EOF
+# custom
+cat <<EOF >"${PROJECT_ROOT_PATH}/out/db/psql-2.properties"
+url=jdbc:postgresql://localhost:5432/testdb
 username=postgres
 password=${POSTGRES_PASSWORD}
 EOF
