3d96ca47fb11fc6143a3f9a52d5ac69a974162c8
caliburn1994 <caliburn1994@gmail.com>
2021-04-26 19:59:25 +0900

    append info

--- deploy/sub/ingress/ingress.sh
+++ deploy/sub/ingress/ingress.sh
@@ -2,4 +2,5 @@ sudo helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 sudo helm repo update
 sudo helm install ingress-nginx ingress-nginx/ingress-nginx
 
-#sudo minikube addons enable ingress
+# after all installed, input this:
+# sudo minikube addons enable ingress
