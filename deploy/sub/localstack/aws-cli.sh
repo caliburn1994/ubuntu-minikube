bb960d8cfdb230bd77c75e1cec1d0eefe1e69141
caliburn1994 <caliburn1994@gmail.com>
2021-05-08 15:47:08 +0900

    install aws sam

--- deploy/sub/localstack/aws-cli.sh
+++ deploy/sub/localstack/aws-cli.sh
@@ -8,16 +8,17 @@ PROJECT_ROOT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../../.. >/dev/null 2>&
 echo_running
 # install https://docs.aws.amazon.com/zh_cn/cli/latest/userguide/install-cliv2-linux.html
 if ! type -p aws &>/dev/null; then
-
   gpg --import aswcliv2-public-key # import public key
-
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip.sig" -o "awscliv2.sig"
-
   if gpg --verify awscliv2.sig awscliv2.zip ; then # if sign is verified， install
     unzip awscliv2.zip
     sudo ./aws/install
   fi
   rm -rf ./aws/install awscliv2.zip awscliv2.sig
 
+  # sam
+  brew tap aws/tap
+  brew install aws-sam-cli
+  sam --version &>/dev/null || echo_warn "failed to install sam "
 fi
