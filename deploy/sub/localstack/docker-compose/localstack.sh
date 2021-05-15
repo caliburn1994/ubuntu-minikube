662fac1bcd99a4a2b8669fea718eb8ccdab48b96
caliburn1994 <caliburn1994@gmail.com>
2021-05-15 20:27:11 +0900

    update

--- deploy/sub/localstack/docker-compose/localstack.sh
+++ deploy/sub/localstack/docker-compose/localstack.sh
@@ -6,11 +6,28 @@ PROJECT_ROOT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../../../.. >/dev/null 
 . "${PROJECT_ROOT_PATH}/deploy/common.sh"
 
 function download() {
-    sudo mkdir -p "/etc/docker-compose/localstack"
-    curl -L https://raw.githubusercontent.com/localstack/localstack/master/docker-compose.yml -o "/etc/docker-compose/localstack/docker-compose.yml"
-    make_service "localstack.service"
+  dc_file="/etc/docker-compose/localstack/docker-compose.yml"
+  ls ${dc_file} >>/dev/null 2>&1 && return 0
+
+  sudo mkdir -p "/etc/docker-compose/localstack"
+  curl -L https://raw.githubusercontent.com/localstack/localstack/master/docker-compose.yml -o "{dc_file}"
+  make_service "localstack.service"
 }
 
+function output_config() {
+  file="${PROJECT_ROOT_PATH}/out/localstack/expose_ip.txt"
+  if [ ! -s $file ]; then
+    echo "http://$(sudo minikube ip):4566" >"$file"
+  fi
+}
+
+function heath_check() {
+  url=$(cat "${PROJECT_ROOT_PATH}/out/localstack/expose_ip.txt")
+  rsl=$(curl -s "$url") # -s silent
+  [[ ${rsl} == '{"status": "running"}' ]] && echo_warn "Localstack was broken"
+}
 
 echo_running
 download
+output_config
+heath_check
